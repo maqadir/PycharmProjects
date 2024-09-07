866c8882c1a3b0afc95ccdc2e999a364efe423e3
maqadir <maqadir@gmail.com>
2024-09-07 23:43:57 +0100

    1st commit

--- pythonProject/Week1.py
+++ pythonProject/Week1.py
@@ -1,4 +1,8 @@
+from enum import unique
+
 data = [10, 20, 31.8, 1, 4, 5, 5, 34, 17, 76, 9]
+
+list_test = [1, 2, 3, 3, 4, 5, 5, 6, 7, 7]
 
 sum_list = 0
 avg_list = 0
@@ -9,3 +13,11 @@ for item in data:
 avg_list = sum_list / len(data)
 
 print('Sum = %d and mean =%.2f' % (sum_list, avg_list))
+
+
+def unique_list(list_test):
+    new_list = list(set(list_test))
+    print(sorted(new_list))
+
+list_test = [1, 2, 3, 3, 4, 5, 5, 6, 7, 7]
+unique_list(list_test)
