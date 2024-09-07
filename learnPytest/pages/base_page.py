8bd57c23e70485c52d4b1140a70ad892b78ba22e
maqadir <31727732+maqadir@users.noreply.github.com>
2024-09-07 19:37:18 +0100

    Add files via upload

---
+++ learnPytest/pages/base_page.py
@@ -0,0 +1,24 @@
+
+class BasePage:
+    """
+    The Purpose Of A BasePage IS TO Contain Methods Common To All Page Objects
+    """
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find(self, *locator):
+        return self.driver.find_element(*locator)
+
+    def click(self, locator):
+        self.find(*locator).click()
+        # self.driver.find_element(*locator).click()
+
+    def set(self, locator, value):
+        self.find(*locator).clear()
+        self.find(*locator).send_keys(value)
+
+    def get_text(self, locator):
+        return self.find(*locator).text
+
+    def get_title(self):
+        return self.driver.title
